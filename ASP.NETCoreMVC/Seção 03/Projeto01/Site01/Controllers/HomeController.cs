68d03f42f1558c66d91dd4f22f225d35f32e156f
Alex Fabiany <alexfabianyleao@yahoo.com.br>
2021-07-11 20:59:21 -0400

    Add project files

---
+++ ASP.NETCoreMVC/Seção 03/Projeto01/Site01/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            //return new ContentResult() { Content = "Olá Mundo!", ContentType = "text/json" };
+
+            return View();
+        }
+    }
+}
